07011f8d49c41a65456dcf514f9d898dc0a2a139
Mats Sommervold <mats.sommervold@gmail.com>
2020-11-20 20:04:27 +0100

    Added initial files

---
+++ src/cli.js
@@ -0,0 +1,22 @@
+import commands from './commands';
+
+const state = {
+    fs: {},
+    path: ['root'],
+    root: '',
+    output: [
+        {}
+    ]
+}
+
+const interact = (command) => {
+    state = commands[command](state);
+    return state;
+}
+
+export const createCli = (structure, root) => {
+    state.fs = typeof structure == 'object' ? structure : state.fs;
+    if(root instanceof Element) state.root = root;
+    else throw new Error('No root element provided');
+    return interact;
+}
---
+++ src/commands.js
@@ -0,0 +1,8 @@
+export const commands = {
+    help: {
+        desc: 'Lists all available commands and their use',
+        action: (state) => {
+            return state;
+        }
+    }
+}
